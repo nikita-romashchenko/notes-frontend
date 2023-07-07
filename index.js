f203eded176452a6ff3435884a61905a256a13f9
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-07-07 20:05:41 +0300

    feat: add ability to create notes

---
+++ index.js
@@ -0,0 +1,67 @@
+let noteViewer
+let notesContainer
+let titleInput
+let descInput
+let noteArr = []
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    noteViewer = document.getElementById("note-viewer")
+    notesContainer = document.getElementById("notes-container")
+    titleInput = document.getElementById('note-title')
+    descInput = document.getElementById('note-description')
+});
+
+function displayNoteViewer() {
+    console.log('displaying')
+    noteViewer.classList.remove('hidden')
+}
+
+function hideNoteViewer() {
+    console.log('hiding')
+
+    noteViewer.classList.add('hidden')
+}
+
+function onAddNoteClick() {
+    console.log('test')
+    displayNoteViewer();
+}
+
+function createNote() {
+    let note = document.createElement('div')
+    note.classList.add('note')
+
+    let noteBtn = document.createElement('div')
+    noteBtn.classList.add("note-btn")
+
+    let title = document.createElement('h1')
+    title.innerHTML = titleInput.value
+
+    let description = document.createElement('p')
+    description.innerHTML = descInput.value
+
+    noteBtn.appendChild(title)
+    noteBtn.appendChild(description)
+    noteBtn.addEventListener('click', () => editNote(noteArr.length))
+
+    note.appendChild(noteBtn)
+    notesContainer.appendChild(note)
+
+    noteArr.push(note)
+
+    titleInput.value = ""
+    descInput.value = ""
+
+    hideNoteViewer()
+
+    console.log(noteArr)
+}
+
+function editNote(noteIndex){
+    titleInput.value = noteArr[noteIndex].get
+    titleInput.value = noteArr[noteIndex].find('p').innerHTML
+
+    displayNoteViewer()
+}
+
+
