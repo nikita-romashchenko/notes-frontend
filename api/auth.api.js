555a978cbd11a3ed7df999666c2d8d8433914aa2
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2023-09-12 18:00:44 +0300

    feat: auth & api WIP

--- api/auth.api.js
+++ api/auth.api.js
@@ -1,10 +1,14 @@
-import { SERVER_ADDRESS } from "../constants";
-import { ACCESS_TOKEN } from "../constants";
+import { SERVER_ADDRESS } from "../constants.js";
+import { ACCESS_TOKEN } from "../constants.js";
 
 export const signUp = async (username, password) => {
+  console.log(JSON.stringify({ username, password }));
   const response = await fetch(`${SERVER_ADDRESS}auth/sign-up`, {
     method: "POST",
     body: JSON.stringify({ username, password }),
+    headers: {
+      "Content-type": "application/json",
+    },
   });
   if (response.ok) {
     return true;
@@ -14,13 +18,15 @@ export const signUp = async (username, password) => {
 
 export const signIn = async (username, password) => {
   const response = await fetch(`${SERVER_ADDRESS}auth/sign-in`, {
-    method: "GET",
+    method: "POST",
     body: JSON.stringify({ username, password }),
+    headers: {
+      "Content-type": "application/json",
+    },
   });
   if (response.ok) {
-    const data = JSON.parse(response.body);
+    const data = await response.json();
     window.localStorage.setItem("ACCESS_TOKEN", data.accessToken);
-    window.localStorage.setItem("REFRESH_TOKEN", data.refreshToken);
     return true;
   }
   return false;
---
+++ auth/auth.controller.js
@@ -0,0 +1,30 @@
+import { signIn, signUp } from "../api/auth.api.js";
+import { ACCESS_TOKEN } from "../constants.js";
+
+const signInUsername = document.getElementById("sign-in-username");
+const signInPassword = document.getElementById("sign-in-password");
+
+const signUpUsername = document.getElementById("sign-up-username");
+const signUpPassword = document.getElementById("sign-up-password");
+
+window.onload = () => {
+  document
+    .getElementById("sign-in-button")
+    .addEventListener("click", () => onSignIn());
+
+  document
+    .getElementById("sign-up-button")
+    .addEventListener("click", () => onSignUp());
+};
+
+const onSignIn = async () => {
+  const isLoggedIn = await signIn(signInUsername.value, signInPassword.value);
+  if (isLoggedIn) {
+    window.location.replace("/");
+  }
+};
+
+const onSignUp = async () => {
+  const isSignedUp = await signUp(signUpUsername.value, signUpPassword.value);
+  console.log(isSignedUp);
+};
